bbfdafe9230feec75ef393ed0a7f36adcc659c52
maskrpone <deparis.hipp@gmail.com>
2023-10-05 09:37:36 +0200

    git management 2

--- src/Entertainment.java
+++ src/Entertainment.java
@@ -1,11 +1,5 @@
 public class Entertainment extends Show {
-    private String speaker;
-
-    public Entertainment() {
-        super("None", 2);
-        this.speaker = "None";
-    }
-
+    private final String speaker;
     public Entertainment(String name, String speaker) {
         super(name, 2);
         this.speaker = speaker;
@@ -15,5 +9,7 @@ public class Entertainment extends Show {
         System.out.println("Name of entertainment : " + this.getName());
         System.out.println("Duration of entertainment : " + this.getDuration());
         System.out.println("Name of the Speaker : " + this.speaker);
+        int djoumalikatou = 0;
+        System.out.println("Djoumalikatou ? : " + djoumalikatou);
     }
 }
