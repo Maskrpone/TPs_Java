2148897bcca53e979f3b678529a74e358cea5271
Maskrpone <90637175+Maskrpone@users.noreply.github.com>
2023-10-13 15:28:28 +0200

    CM 12/10 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Notes notes = new Notes();
+        int[] toTest = new int[]{15, -15, 17, 11, 14};
+
+        for (int j : toTest) {
+            try {
+                notes.addNote(j);
+            } catch (ErrorExceptions e) {
+                System.out.println("Error:" + e.getMessage());
+            }
+        }
+        notes.getNotes();
     }
 }
